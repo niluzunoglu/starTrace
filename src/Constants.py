038c1a986106fac8d6893bdcf91653b20f8dd76f
Nil Uzunoglu <nily.uzun17@gmail.com>
2025-03-16 16:16:03 +0300

    CHG: Data

--- src/Constants.py
+++ src/Constants.py
@@ -1,4 +1,4 @@
 # Constants.py
 EXOPLANET_DATA_FILE = "../data/cumulative.csv"
-CONFIRMED_EXOPLANET_DATA_FILE = "../data/labeled/kepler_confirmed.csv"
-FALSE_POSITIVE_EXOPLANET_DATA_FILE = "../data/labeled/kepler_false_positives.csv"
+CONFIRMED_EXOPLANET_DATA_FILE = "../data/labeled/kepler_confirmed_exoplanets.csv"
+FALSE_POSITIVE_EXOPLANET_DATA_FILE = "../data/labeled/kepler_confirmed_false_positives.csv"
