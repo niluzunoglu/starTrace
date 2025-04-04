d82dc9b8c0395b643cab982f5632d4f03c137e20
A. Nil Uzunoglu <47059818+niluzunoglu@users.noreply.github.com>
2025-04-05 00:25:23 +0300

    UPD: Constants.py

--- src/Constants.py
+++ src/Constants.py
@@ -6,3 +6,7 @@ FALSE_POSITIVE_EXOPLANET_DATA_FILE = "../data/labeled/kepler_confirmed_false_pos
 KEPLER_CSV_PATH = "../data/multimission/KEPLER/cumulative.csv"
 TESS_CSV_PATH = "../data/multimission/TESS/TOI_2025.03.23_03.49.58.csv"
 MERGED_CSV_PATH = "../data/multimission/MERGED/kepler_and_tess.csv"
+
+KEPLER_COLAB_DATA_PATH = "/content/drive/MyDrive/starTrace/data/multimission/KEPLER/cumulative.csv"
+TESS_COLAB_DATA_PATH = "/content/drive/MyDrive/starTrace/data/multimission/TESS/TOI_2025.03.23_03.49.58.csv"
+MERGED_COLAB_DATA_PATH = "/content/drive/MyDrive/starTrace/data/multimission/MERGED/kepler_and_tess_from_colab.csv"
